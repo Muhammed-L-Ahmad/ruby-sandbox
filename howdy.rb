a4379cbd68d76ed85d8478825405803f18bda4d9
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-03-24 19:00:24 +0000

    ordinalizing

--- howdy.rb
+++ howdy.rb
@@ -1,5 +1,12 @@
 # This is a comment
+require "active_support/all"
+
 my_string = "Hello, world!"
 pp my_string
 
 require "./goodbye.rb"
+
+1.ordinalize
+3.ordinalize
+53.ordinalize
+2999.ordinalize
